6b6a9ccb84dc1108e6ac1a29a0349467bd3c2c98
Gaby <GrabyAprende@gmail.com>
2024-03-06 19:04:38 +0100

    Ajustes en landing page

--- src/app/components/form/logo.tsx
+++ src/app/components/form/logo.tsx
@@ -1,11 +1,8 @@
-{/*import { useContext, type FC } from "react";
+import type { FC } from "react";
 import Image from "next/image";
-import { LayoutContext } from "@/layout/context/layoutcontext";
 
 export const Logo: FC = () => {
-    const { layoutConfig } = useContext(LayoutContext);
-
     return (
-        <Image fill src={`/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Sakai logo" className="relative mb-5 w-6rem flex-shrink-0" />
+        <Image fill src={`/layout/images/logo.png`} alt="Wallaclone logo" className="relative w-8rem flex-shrink-0" />
     );
-};*/}
+};
