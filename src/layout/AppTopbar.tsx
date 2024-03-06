bf2bcb52ded6fda9fe60a17233b94bdc6e6d9b5d
Gaby <GrabyAprende@gmail.com>
2024-03-06 18:48:36 +0100

    uso de Image para evitar errores

--- src/layout/AppTopbar.tsx
+++ src/layout/AppTopbar.tsx
@@ -5,6 +5,7 @@ import { classNames } from 'primereact/utils';
 import React, { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
 import { AppTopbarRef } from '@/types';
 import { LayoutContext } from './context/layoutcontext';
+import Image from 'next/image';
 
 const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
     const { layoutConfig, layoutState, onMenuToggle, showProfileSidebar } = useContext(LayoutContext);
@@ -21,8 +22,8 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
     return (
         <div className="layout-topbar">
             <Link href="/" className="layout-topbar-logo">
-                <img src={`/layout/images/logo-${layoutConfig.colorScheme !== 'light' ? 'white' : 'dark'}.svg`} width="47.22px" height={'35px'} alt="logo" />
-                <span>SAKAI</span>
+                <Image src={`/layout/images/logo${layoutConfig.colorScheme}.png`} width="47.22" height={'35'} alt="wallaclone logo" />
+                <span>Wallaclone</span>
             </Link>
 
             <button ref={menubuttonRef} type="button" className="p-link layout-menu-button layout-topbar-button" onClick={onMenuToggle}>
