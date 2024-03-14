e735858c08fbffc94c46d010e10885c4fd74bdbd
Dulce Flores <dulce.1201@gmail.com>
2024-03-14 20:19:11 +0100

    Improve structure of advert detail

--- src/app/anuncios/[id]/page.tsx
+++ src/app/anuncios/[id]/page.tsx
@@ -1,14 +1,26 @@
+"use client";
+
 import { Anuncio } from "@/types/general.types";
 import { FC } from "react";
-import React, { useContext, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { classNames } from "primereact/utils";
 import { Button } from "primereact/button";
-import Image from "next/image";
+import { Tag } from "primereact/tag";
+import { Avatar } from "primereact/avatar";
+//import Image from "next/image";
 
 interface Props {
     params: {
         id: string;
     };
+}
+
+async function getData(id: string) {
+    const res = await fetch(`http://35.169.246.52/api/advert/id/${id}`);
+    if (!res.ok) {
+        console.log(Error);
+    }
+    return res.json();
 }
 
 const productDetails = {
@@ -20,12 +32,36 @@ const productDetails = {
     tags: ["Smartphones", "New"],
 };
 
+const tags = productDetails.tags.map((tag, index) => (
+    <Tag key={index} value={tag} className="p-element">
+        <span className="p-tag p-component p-tag-rounded">
+            <span className="p-tag-value"></span>
+        </span>
+    </Tag>
+));
+
 const AdvertDetail: FC<Props> = ({ params }) => {
+    const [product, setProduct] = useState<Anuncio | null>(null);
+
     return (
         <div className="align-items-center flex justify-content-center lg:px-8 md:px-6 px-4 py-8 surface-ground ng-star-inserted">
             <div className="shadow-2 p-3 h-full flex flex-column surface-card lg:w-7">
                 <div>
-                    <h6>User: {productDetails.seller}</h6>
+                    <Avatar
+                        size="large"
+                        shape="circle"
+                        className="p-element mb-3"
+                    >
+                        <div
+                            data-pc-name="avatar"
+                            className="p-avatar p-component p-avatar-circle p-avatar-l"
+                        >
+                            <span className="p-avatar-text ng-star-inserted">
+                                U
+                            </span>
+                        </div>
+                    </Avatar>
+                    <span className="px-3">{productDetails.seller}</span>
                     <span>{params.id}</span>
                 </div>
 
@@ -69,24 +105,24 @@ const AdvertDetail: FC<Props> = ({ params }) => {
                 </div>
                 <p className="mt-0 mb-3 text-600 line-height-3">
                     {productDetails.description}
-                    <p>Seller: {productDetails.seller}</p>
-                    <p>Tags: {productDetails.tags.join(", ")}</p>
                 </p>
+                <div className="flex flex-wrap gap-2">{tags}</div>
                 <hr className="my-3 mx-0 border-top-1 border-none surface-border" />
                 <ul className="list-none p-0 m-0 flex-grow-1">
                     <li className="flex align-items-center mb-3">
                         <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                        <span>Arcu vitae elementum</span>
+                        <span>En punto de recogidadesde 2.59 EUR</span>
                     </li>
                     <li className="flex align-items-center mb-3">
                         <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                        <span>Dui faucibus in ornare</span>
-                    </li>
-                    <li className="flex align-items-center mb-3">
-                        <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                        <span>Morbi tincidunt augue</span>
+                        <span>En mi direccióndesde 3.49 EUR</span>
                     </li>
                 </ul>
+                <hr className="mb-3 mx-0 border-top-1 border-none surface-border mt-auto" />
+                <div className="flex align-items-center mb-3">
+                    <i className="pi pi-check-circle text-green-500 mr-2"></i>
+                    <h4>Protección Wallaclone</h4>
+                </div>
                 <Button label="Buy Now" className="p-3 w-full mt-auto"></Button>
             </div>
         </div>
